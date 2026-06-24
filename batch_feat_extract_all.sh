20c8cff60d1b6a36b8a0a4654f9eea025c25b0f3
AlexvndreLs <alexandre.louis@etu.minesparis.psl.eu>
2026-06-24 15:23:47 -0700

    aaa

--- batch_feat_extract_all.sh
+++ batch_feat_extract_all.sh
@@ -13,6 +13,12 @@
 #   1 -> preprocessed-ica     -> dream_features
 #   2 -> preprocessed-noica   -> dream_features_noica
 #   3 -> preprocessed-iclabel -> dream_features_iclabel
+#
+# Note --overwrite : ne pas ajouter en production. feat_extract skippe
+# automatiquement les .npz existants (cache atomique par sujet/stade).
+# N'utiliser --overwrite que si les .npz sont obsoletes (ex: changement
+# de reference EEG). Dans ce cas, renommer d'abord les anciens dossiers
+# en _CAR (archive), puis relancer sans --overwrite.
 
 DERIV_ROOT=/home/alouis/scratch/dream_bids/derivatives
 SAVE_ROOT=/home/alouis/scratch
@@ -37,8 +43,8 @@ echo "Job ${SLURM_JOB_ID} array ${SLURM_ARRAY_TASK_ID} on $(hostname)"
 echo "Start: $(date)"
 
 mkdir -p ${SAVE_ROOT}/${SAVE}
-
 cd /home/alouis/dream-recall-alex
+
 /home/alouis/mne_env/bin/python feat_extract_umap_fooof_v4.py \
     --deriv-path ${DERIV_ROOT}/${BRANCH} \
     --save-path  ${SAVE_ROOT}/${SAVE} \
